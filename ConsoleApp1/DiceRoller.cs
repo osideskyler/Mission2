f76841032a6ceef1d6dae97fe1a47208f7898434
osideskyler <osidesky@byu.edu>
2026-01-12 11:45:00 -0700

    add comments

--- ConsoleApp1/DiceRoller.cs
+++ ConsoleApp1/DiceRoller.cs
@@ -2,13 +2,16 @@ namespace ConsoleApp1;
 
 public class DiceRoller
 {
+    //method for rolling dice
     public int[] RollDice(int numRolls)
     {
+        //make array 0 - 12 indexes, wont use 0 and 1
         int[] results = new int[13];
         Random random = new Random();
 
         for (int i = 0; i < numRolls; i++)
         {
+            //randomly generate dice
             int die1 = random.Next(1, 7);
             int die2 = random.Next(1, 7);
             int sum = die1 + die2;
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,24 +4,22 @@ class Program
 {
     static void Main()
     {
+        //ask user for num rolls
         Console.WriteLine("Welcome to the dice throwing simulator!");
         Console.WriteLine("How many dice would you like to simulate?");
         int numRolls = int.Parse(Console.ReadLine());
-
+        //create roller instance and run rolls to create array
         DiceRoller roller = new DiceRoller();
         int[] results = roller.RollDice(numRolls);
         
         Console.WriteLine("\nDice Roll Results\n");
-
+        //loops through all dice results
         for (int i = 2; i <= 12; i++)
         {
             double percentage = (double)results[i] / numRolls * 100;
             int stars = (int)Math.Round(percentage);
-
+            //print histogram
             Console.WriteLine($"{i}: " + new string('*', stars));
         }
-        
-
-
     }
 }
